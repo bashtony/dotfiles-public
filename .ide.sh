71d0cb11cc23f5879d089d02a234f4f807da5a64
Antônio Paulo <antoniopaulo404ap@gmail.com>
2025-05-29 12:47:39 -0400

    Update .ide.sh

--- .ide.sh
+++ .ide.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
-cd Projetos/ReactJS/
-echo "projetos atuais: $(ls -d *)"
-read -p "Nome do projeto: " projeto
+
+diretorio="Projetos/ReactJS/"
 viteconfig="import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({ plugins: [ tailwindcss(), ], })"
 indexcss='@import "tailwindcss";'
 
-if [ -d $projeto ]
-  then
-		mkdir $projeto; cd ./$projeto; tmux
-	else
-		mkdir $projeto; cd ./$projeto; npm create vite@latest ./; npm install; npm install tailwindcss @tailwindcss/vite
+if [ ! -d "$diretorio" ]; then
+  mkdir -p "$diretorio"
+fi
+cd "$diretorio"
+
+read -p "Nome do projeto: " projeto
+if [ -z "$projeto" ]; then
+	echo "Nome do projeto não foi fornecido. Saindo..."
+	exit 1
+fi
+
+if [ -d $projeto ]; then
+	cd "$projeto"
+	tmux
+else
+  mkdir $projeto; cd ./$projeto; npm create vite@latest ./; npm install; npm install tailwindcss @tailwindcss/vite
 		echo "$viteconfig" > vite.config.js
 		cd src; echo "$indexcss" > index.css
 		cd ../
 		tmux
 fi
-
